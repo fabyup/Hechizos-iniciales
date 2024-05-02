1cf42df024ea1d30d858eb852bad55e303e674ce
fabyup <afabyblasco@gmail.com>
2024-05-02 17:18:28 -0300

    Este es el ejercicio 4 secuencial

--- Hechizos nivel 1 - Secuencial/ej4.js
+++ Hechizos nivel 1 - Secuencial/ej4.js
@@ -9,9 +9,17 @@ const leer = require("prompt-sync")();
  */
 
 
-function main() {
+function main(magoEdad,diasHechizoPatronus) {
+    let edad= 25;
+    let diasDePractica=158;
+    let potencia= 129;
     
+    
+    console.log("Ingresa la edad del mago y la cantidad de dias de practicas del hechizo Patronus");
+     console.log("La edad del mago es.." +edad);
+     console.log("Los dias de practica son..."+ diasDePractica);
+     console.log("La potencia es.."+ potencia);
 }
 
 
-main();
+main(25,158);
