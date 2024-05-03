dac17fb08e1cbca99e1d0f0be418292f297c98cd
fabyup <afabyblasco@gmail.com>
2024-05-02 21:13:19 -0300

    este es el ejercicio 2 selectivo

--- Hechizos nivel 2 - Selectivo/if - else if - else/ej2.js
+++ Hechizos nivel 2 - Selectivo/if - else if - else/ej2.js
@@ -1,21 +1,24 @@
 const leer = require("prompt-sync")();
 
 /**
- * Ej 2 
+ * Ej 2
  * Elección de Casa en Hogwarts:
  * Solicita al estudiante su puntuación en un examen y determina a qué casa de Hogwarts pertenecería.
  * Gryffindor - a partir de 90 puntos
  * Hufflepuff - a partir de 70 puntos
  * Ravenclaw - a partir de 50 puntos
  * Slytherin - menos de 50 puntos
- * 
+ *
  * Por ejemplo si el estudiante saco 100 puntos entonces pertenece a Gryffindor o si sacara 30 entonces pertenece a Slytherin
  */
 
-
-function main() {
-    
+function main(Puntuacion) {
+  console.log("Podrias decirme cual fue tu puntuacion en el examen por favor");
+  let ingresarPuntuacion1 = Number(leer());
+  if (ingresarPuntuacion1 >=50) {
+    console.log("Pertenece a la casa Ravenclaw");
+  }
 }
-
-
 main();
+
+
