221d581505fc2e918bcd44cf34ad0d9dbe1a38e5
fabyup <afabyblasco@gmail.com>
2024-06-06 16:11:42 -0300

    Ejercicio 2 de for sin terminar

--- Hechizos nivel 3 - Iterativo/1 - for/ej2.js
+++ Hechizos nivel 3 - Iterativo/1 - for/ej2.js
@@ -3,39 +3,39 @@ const leer = require("prompt-sync")();
 /**
  * Ej 2
  * Los Dementores son criaturas oscuras que se alimentan de
- *  la felicidad de las personas, dejándolas con recuerdos dolorosos y 
- * desesperación. Para protegerse de los Dementores, los estudiantes de Hogwarts aprenden 
- * el hechizo Patronus, que repele a estas criaturas y los aleja con recuerdos felices. En este 
- * ejercicio, simularemos un encuentro con un Dementor en un mini juego donde un estudiante debe 
+ *  la felicidad de las personas, dejándolas con recuerdos dolorosos y
+ * desesperación. Para protegerse de los Dementores, los estudiantes de Hogwarts aprenden
+ * el hechizo Patronus, que repele a estas criaturas y los aleja con recuerdos felices. En este
+ * ejercicio, simularemos un encuentro con un Dementor en un mini juego donde un estudiante debe
  * lanzar el hechizo Patronus correctamente para repeler al Dementor y proteger su felicidad. Dependiendo
  *  de la cantidad de vida del estudiante, se necesitará ingresar el hechizo Patronus varias veces para
- *  vencer al Dementor. Si el estudiante no ingresa el hechizo correctamente, perderá turnos y 
- * el Dementor consumirá su felicidad y recuerdos felices, acercándose más a la derrota del estudiante. 
+ *  vencer al Dementor. Si el estudiante no ingresa el hechizo correctamente, perderá turnos y
+ * el Dementor consumirá su felicidad y recuerdos felices, acercándose más a la derrota del estudiante.
  * En otro caso si el estudiante ingresa bien los hechizos y logra vencer al dementor entonces se
  *  mostrar un mensaje de victoria
- * 
+ *
  * Vida maxima del dementor: 1000
  * Vida maxima del estudiante: 350
  * Daño por turno perdido del dementor al estudiante: 75
  * Daño por hechizo acertado del estudiante contra dementor: 267
- * Turnos totales: 5 
+ * Turnos totales: 5
  */
 
-
 function main() {
-   let vidaMaximaDeDementor=1000
-   let hechizoAcertado=267
+  console.log("ingresa hechizo");
 
-    for(let hechizoPatronus=1;hechizoPatronus<=5;hechizoPatronus++){
-     if(vidaMaximaDeDementor-hechizoAcertado ){
-        console.log(hechizoPatronus,"");
-
-     }
-
-
+  for (
+    let hechizoPatronusLanzados = 0;
+    hechizoPatronusLanzados < 3;
+    hechizoPatronusLanzados++
+  ) {
+    let intentos = leer();
+    if ((hechizoPatronusLanzados = hechizoPatronusLanzados)) {
+      console.log("Acertaste el dementor tiene menos vida");
+    } else if ((intentos = intentos)) {
+      console.log("El dementor esta avanzando");
     }
-    
+  }
 }
-
 
 main();
